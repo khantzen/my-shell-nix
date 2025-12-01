b6089b78987dd6a1d687a72de01d9d87dd6e646d
kha20280826 <kevin.hantzen@afp-ext.com>
2025-12-01 16:15:52 +0100

    add shell nix with jq and yq

---
+++ shell.data_parsing.nix
@@ -0,0 +1,26 @@
+let
+pkgs = import <nixpkgs> {};
+
+pythonEnv = pkgs.python312;
+
+workTools = with pkgs; [
+  cowsay
+  fortune
+];
+
+shell = pkgs.mkShell {
+  name = "Ansible Project"
+
+  nativeBuildInputs = [ 
+    pkgs.yq
+    pkgs.jq
+    workTools 
+  ];
+
+  shellHook = ''
+    fortune | cowsay
+  '';
+};
+
+in
+shell
